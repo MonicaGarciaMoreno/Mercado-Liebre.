85fc8eea8889bdae114f7d59daa192a6116b6e77
MonicaGarciaMoreno <monicagarcia1122@hotmail.com>
2021-10-27 20:07:44 -0500

    Se actualiza la entrega de Mercado Liebre

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+const app = express();
+const path = require('path')
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))});
+
+app.get('/register.html', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'./views/register.html'))});
+
+app.get('/login.html', (req, res) => {
+            res.sendFile(path.resolve(__dirname,'./views/login.html'))});
+
+app.listen(process.env.PORT||3000,function(){
+    console.log("Servidor corriendo");
+})
